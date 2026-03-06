66e09bd5933438ea5e89355a0a8659eb4c0405dc
Đỗ Hoàng Vũ <vuker12345@gmail.com>
2026-03-07 00:01:55 +0700

    tải hbs và cấu hình trong app..js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { engine } = require('express-handlebars')
+const path = require('path')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+app.engine(
+    '.hbs',
+    engine({
+        extname: '.hbs',
+        layoutsDir: path.join(__dirname, 'views/layouts'),
+        partialsDir: path.join(__dirname, 'views/partials'),
+        defaultLayout: 'main',
+
+        helpers: {
+            json: (x) => JSON.stringify(x),
+            eq: (a, b) => a === b,
+            formatPrice: (v) => v.toLocaleString(),
+            multiply: (a,b) => a*b,
+        },
+    }),
+)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
