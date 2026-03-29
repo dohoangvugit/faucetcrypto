267392c888c07f4438cd6148f459120037f91221
Đỗ Hoàng Vũ <vuker12345@gmail.com>
2026-03-29 10:07:06 +0700

    hoàn thành giao diện form đăng nhập đăng kí

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const homeRoute = require('../routes/homeRoute')
 const taskRoute = require('../routes/taskRoute')
+const authRoute = require('../routes/authRoute')
 
 function route (app){
     
     app.use('/admin/task', taskRoute)
     app.use('/', homeRoute)
+    app.use('/', authRoute)
+
 }
 
 module.exports = route
